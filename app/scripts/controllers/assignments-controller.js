8b264c41534b148b994d7b9afeac7cca36474a4a
Chakradhar Katta <chakradhar.katta@gmail.com>
2017-01-23 09:51:40 +0000

    Notice board fix by Dinesh on 23-01-2017

--- app/scripts/controllers/assignments-controller.js
+++ app/scripts/controllers/assignments-controller.js
@@ -8,112 +8,112 @@
  * Controller of the studymonitorApp
  */
 angular.module('studymonitorApp')
-    .controller('AssignmentsController', function (assignmentsService, $cookies, $timeout) {
+    .controller('AssignmentsController', function(assignmentsService, $cookies, $timeout) {
         var AssignmentsCtrl = this;
         //Get Assignment details by School ID
         AssignmentsCtrl.schoolId = $cookies.getObject('uds').schoolId;
 
         function init() {
 
-            this.getAssignmentDetails = function () {
-                assignmentsService.getAssignmentDetailsBySchoolId(AssignmentsCtrl.schoolId).then(function (result) {
+            this.getAssignmentDetails = function() {
+                assignmentsService.getAssignmentDetailsBySchoolId(AssignmentsCtrl.schoolId).then(function(result) {
                     if (result) {
                         AssignmentsCtrl.assignmentList = result;
-
-                        $timeout(function () {
-                            var columnsDefs = [null, null, {
-                                "width": "30%"
-                            }, null, null, {
-                                    "orderable": false,
-                                    "width": "10%",
-                                    "targets": 0
-                                }, {
-                                    "orderable": false,
-                                    "width": "10%",
-                                    "targets": 0
-                                }, {
-                                    "orderable": false,
-                                    "width": "10%",
-                                    "targets": 0
-                                }];
-                            TableEditable.init("#assignments_datatable", columnsDefs);
-                            Metronic.init();
-                        });
                     }
-                }, function (error) {
+                }, function(error) {
                     console.log('Error while fetching the assignment records. Error stack : ' + error);
                 });
             }
         }
         (new init()).getAssignmentDetails();
+        //Initialize the Table Component
+        $timeout(function() {
+            var columnsDefs = [null, null, {
+                "width": "30%"
+            }, null, null, {
+                "orderable": false,
+                "width": "10%",
+                "targets": 0
+            }, {
+                "orderable": false,
+                "width": "10%",
+                "targets": 0
+            }, {
+                "orderable": false,
+                "width": "10%",
+                "targets": 0
+            }];
+            TableEditable.init("#assignments_datatable", columnsDefs);
+            Metronic.init();
+        });
         //Close or Open modal
-        AssignmentsCtrl.closeModal = function () {
+        AssignmentsCtrl.closeModal = function() {
             var modal = $('#edit-assignments');
             modal.modal('hide');
 
             //ClearFields
             clearformfields();
         }
-        AssignmentsCtrl.openModal = function () {
-            var modal = $('#edit-assignments');
-            modal.modal('show');
-        }
-        //Clear Fields
+        AssignmentsCtrl.openModal = function() {
+                var modal = $('#edit-assignments');
+                modal.modal('show');
+            }
+            //Clear Fields
         function clearformfields() {
             AssignmentsCtrl.formFields = {};
         }
         //Delete confirmation box
-      AssignmentsCtrl.confirmCallbackMethod = function (index) {
-          deleteAssignment(index);
-      }
-      //Delete cancel box
-      AssignmentsCtrl.confirmCallbackCancel = function (index) {
-          return false;
-      }
-        // Add Action
-        AssignmentsCtrl.assignmentAction = function (invalid) {
-            if (invalid) {
-                return;
+        AssignmentsCtrl.confirmCallbackMethod = function(index) {
+                deleteAssignment(index);
             }
-            var data = {
-                schoolId: AssignmentsCtrl.schoolId,
-                title: AssignmentsCtrl.formFields.title,
-                classId: AssignmentsCtrl.formFields.classId,
-                description: AssignmentsCtrl.formFields.description,
-                fromDate: AssignmentsCtrl.formFields.fromDate,
-                toDate: AssignmentsCtrl.formFields.toDate
+            //Delete cancel box
+        AssignmentsCtrl.confirmCallbackCancel = function(index) {
+                return false;
             }
-            if (data) {
-                assignmentsService.getExistingAssignmentRecords(data).then(function (result) {
-                    if (result) {
-                        console.log('data already exists');
-                        return;
-                    }
-                }, function (result1) {
-                    assignmentsService.CreateOrUpdateAssignment(data).then(function (res) {
-                        if (res) {
-                            (new init()).getAssignmentDetails();
-                            AssignmentsCtrl.closeModal();
+            // Add Action
+        AssignmentsCtrl.assignmentAction = function(invalid) {
+                if (invalid) {
+                    return;
+                }
+                var data = {
+                    schoolId: AssignmentsCtrl.schoolId,
+                    title: AssignmentsCtrl.formFields.title,
+                    classId: AssignmentsCtrl.formFields.classId,
+                    description: AssignmentsCtrl.formFields.description,
+                    fromDate: AssignmentsCtrl.formFields.fromDate,
+                    toDate: AssignmentsCtrl.formFields.toDate
+                }
+                if (data) {
+                    assignmentsService.getExistingAssignmentRecords(data).then(function(result) {
+                        if (result) {
+                            console.log('data already exists');
+                            return;
                         }
+                    }, function(result1) {
+                        assignmentsService.CreateOrUpdateAssignment(data).then(function(res) {
+                            if (res) {
+                                (new init()).getAssignmentDetails();
+                                AssignmentsCtrl.closeModal();
+                            }
 
-                    }, function (error) {
-                        console.log("Error while Fetching the Records" + error);
+                        }, function(error) {
+                            console.log("Error while Fetching the Records" + error);
+                        });
                     });
+                }
+            }
+            //Delete Action
+        var deleteAssignment = function(index) {
+            if (AssignmentsCtrl.assignmentList) {
+                assignmentsService.deleteAssignment(AssignmentsCtrl.assignmentList[index].id).then(function(result) {
+                    if (result) {
+                        //On Successfull refill the data list
+                        (new init()).getAssignmentDetails();
+                        AssignmentsCtrl.closeModal();
+                    }
+                }, function(error) {
+                    console.log('Error while deleting class. Error Stack' + error);
                 });
             }
         }
-         //Delete Action
-      var deleteAssignment = function (index) {
-          if (AssignmentsCtrl.assignmentList) {
-              assignmentsService.deleteAssignment(AssignmentsCtrl.assignmentList[index].id).then(function (result) {
-                  if (result) {
-                      //On Successfull refill the data list
-                      (new init()).getAssignmentDetails();
-                      AssignmentsCtrl.closeModal();
-                  }
-              }, function (error) {
-                  console.log('Error while deleting class. Error Stack' + error);
-              });
-          }
-      }
     });
--- app/scripts/controllers/noticeboard-controller.js
+++ app/scripts/controllers/noticeboard-controller.js
@@ -9,109 +9,116 @@
  * Controller of the studymonitorApp
  */
 angular.module('studymonitorApp')
-    angular.module('studymonitorApp')
-  .controller('NoticeboardController', function (noticeboardService, $cookies, $timeout) {
-      var NoticeboardCtrl = this;
-      NoticeboardCtrl.schoolId = $cookies.getObject('uds').schoolId;
-      function init() {
-          this.getNoticeDetails = function () {
-              noticeboardService.getNoticeDetailsBySchoolId(NoticeboardCtrl.schoolId).then(function (result) {
-                  if (result) {
-                      NoticeboardCtrl.noticeList = result;
+angular.module('studymonitorApp')
+    .controller('NoticeboardController', function(noticeboardService, $cookies, $timeout) {
+        var NoticeboardCtrl = this;
+        NoticeboardCtrl.schoolId = $cookies.getObject('uds').schoolId;
 
-                        $timeout(function () {
-                            var columnsDefs = [null, null, {
-                                "width": "30%"
-                            }, null, null, {
-                                    "orderable": false,
-                                    "width": "10%",
-                                    "targets": 0
-                                }, {
-                                    "orderable": false,
-                                    "width": "10%",
-                                    "targets": 0
-                                }, {
-                                    "orderable": false,
-                                    "width": "10%",
-                                    "targets": 0
-                                }];
-                            TableEditable.init("#notice_datatable", columnsDefs);
-                            Metronic.init();
-                        });
+        function init() {
+            this.getNoticeDetails = function() {
+                noticeboardService.getNoticeDetailsBySchoolId(NoticeboardCtrl.schoolId).then(function(result) {
+                    if (result) {
+                        NoticeboardCtrl.noticeList = result;
                     }
-                }, function (error) {
+                }, function(error) {
                     console.log('Error while fetching the assignment records. Error stack : ' + error);
                 });
             }
         }
         (new init()).getNoticeDetails();
+        //Initialize the Table Component
+        $timeout(function() {
+            var columnsDefs = [{
+                "width":"10%"
+            }, {
+                "width":"40%"
+            }, {
+                "width": "10%"
+            }, {
+                "width":"10%"
+            }, {
+                "orderable": false,
+                "width": "10%",
+                "targets": 0
+            }, {
+                "orderable": false,
+                "width": "10%",
+                "targets": 0
+            }, {
+                "orderable": false,
+                "width": "10%",
+                "targets": 0
+            }];
+            TableEditable.init("#notice_datatable", columnsDefs);
+            Metronic.init();
+        });
         //Close or Open modal
-       NoticeboardCtrl.closeModal = function () {
+        NoticeboardCtrl.closeModal = function() {
             var modal = $('#edit-notice');
             modal.modal('hide');
 
             //ClearFields
             clearformfields();
         }
-       NoticeboardCtrl.openModal = function () {
-            var modal = $('#edit-notice');
-            modal.modal('show');
-        }
-        //Clear Fields
+        NoticeboardCtrl.openModal = function() {
+                var modal = $('#edit-notice');
+                modal.modal('show');
+            }
+            //Clear Fields
         function clearformfields() {
-           NoticeboardCtrl.formFields = {};
+            NoticeboardCtrl.formFields = {};
         }
         //Delete confirmation box
-     NoticeboardCtrl.confirmCallbackMethod = function (index) {
-          deleteNotice(index);
-      }
-      //Delete cancel box
-     NoticeboardCtrl.confirmCallbackCancel = function (index) {
-          return false;
-      }
-        // Add Action
-       NoticeboardCtrl.noticeboardAction = function (invalid) {
-            if (invalid) {
-                return;
+        NoticeboardCtrl.confirmCallbackMethod = function(index) {
+                deleteNotice(index);
             }
-            var data = {
-                schoolId:NoticeboardCtrl.schoolId,
-                title:NoticeboardCtrl.formFields.title,
-                description:NoticeboardCtrl.formFields.description,
-                date1:NoticeboardCtrl.formFields.date1,
-                date2:NoticeboardCtrl.formFields.date2
+            //Delete cancel box
+        NoticeboardCtrl.confirmCallbackCancel = function(index) {
+                return false;
             }
-            if (data) {
-                noticeboardService.getExistingNoticeRecords(data).then(function (result) {
-                    if (result) {
-                        console.log('data already exists');
-                        return;
-                    }
-                }, function (result1) {
-                    noticeboardService.CreateOrUpdateNoticeboard(data).then(function (res) {
-                        if (res) {
-                            (new init()).getNoticeDetails();
-                           NoticeboardCtrl.closeModal();
+            // Add Action
+        NoticeboardCtrl.noticeboardAction = function(invalid) {
+                if (invalid) {
+                    return;
+                }
+                var data = {
+                    schoolId: NoticeboardCtrl.schoolId,
+                    title: NoticeboardCtrl.formFields.title,
+                    description: NoticeboardCtrl.formFields.description,
+                    date1: NoticeboardCtrl.formFields.date1,
+                    date2: NoticeboardCtrl.formFields.date2
+                }
+                if (data) {
+                    noticeboardService.getExistingNoticeRecords(data).then(function(result) {
+                        if (result) {
+                            console.log('data already exists');
+                            return;
                         }
+                    }, function(result1) {
+                        noticeboardService.CreateOrUpdateNoticeboard(data).then(function(res) {
+                            if (res) {
+                                (new init()).getNoticeDetails();
+                                NoticeboardCtrl.closeModal();
+                            }
 
-                    }, function (error) {
-                        console.log("Error while Fetching the Records" + error);
+                        }, function(error) {
+                            console.log("Error while Fetching the Records" + error);
+                        });
                     });
+                }
+            }
+            //Delete Action
+        var deleteNotice = function(index) {
+            if (NoticeboardCtrl.noticeList) {
+                noticeboardService.deleteNotice(NoticeboardCtrl.noticeList[index].id).then(function(result) {
+                    if (result) {
+                        //On Successfull refill the data list
+                        (new init()).getNoticeDetails();
+                        NoticeboardCtrl.closeModal();
+                    }
+                }, function(error) {
+                    console.log('Error while deleting class. Error Stack' + error);
                 });
             }
         }
-         //Delete Action
-      var deleteNotice = function (index) {
-          if (NoticeboardCtrl.noticeList) {
-              noticeboardService.deleteNotice(NoticeboardCtrl.noticeList[index].id).then(function (result) {
-                  if (result) {
-                      //On Successfull refill the data list
-                      (new init()).getNoticeDetails();
-                     NoticeboardCtrl.closeModal();
-                  }
-              }, function (error) {
-                  console.log('Error while deleting class. Error Stack' + error);
-              });
-          }
-      }
     });
